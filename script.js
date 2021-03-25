bac3dcc3e54f1ecb6979ee8da9462e768392d01e
Eloi-Perez <eloi104@hotmail.com>
2021-03-25 10:04:19 +0000

    adding items to list

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-li.on("click", function () {
-    li.addClass("strike");
-});
+// li.on("click", function () {
+//     li.addClass("strike");
+// });
+
+
+function newItem() {
+    //1. Adding a new item to the list of items:
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        $('#list').append(li);
+    }
+    
+}
